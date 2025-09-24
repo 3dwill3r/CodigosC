ef9615cc5154d0e857961eca9946959854d78104
Bipo <143854668+3dwill3r@users.noreply.github.com>
2025-09-24 15:15:42 -0500

    codigo em c

---
+++ codigos/sequenciacrescente.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, i;
+    scanf("%d", &x);
+    while(x){
+        for (i = 1; i < x; i++){
+            printf("%d ", i);
+        }
+
+        printf("%d\n", x);
+        scanf("%d", &x);
+    }
+
+    return 0;
+}
