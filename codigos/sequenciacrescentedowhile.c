859c59f16edd5d8c0a55684401cc3e0b4f8570bb
Bipo <143854668+3dwill3r@users.noreply.github.com>
2025-09-24 15:20:42 -0500

    codigo em c

---
+++ codigos/sequenciacrescentedowhile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x, i;
+    do{
+        scanf("%d", &x);
+        for (i = 1; i < x; i++){
+            printf("%d ", i);
+        }
+        if(x)printf("%d\n", x);
+        
+    }while(x)
+
+    return 0;
+}
