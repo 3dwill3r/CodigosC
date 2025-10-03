b0432106a04fd537572fadef4891d236b5175a86
Bipo <143854668+3dwill3r@users.noreply.github.com>
2025-10-03 15:50:22 -0500

    codigo em c

--- codigos/media.c
+++ codigos/media.c
@@ -4,7 +4,7 @@ int main()
 {
     double pesoa = 3.5 ,pesob = 7.5, a, b, mediap;
     scanf("%lf%lf", &a, &b);
-    mediap = ((a * pesoa) + (b * pesob)) / (pesoa + pesob);
+    mediap = ((a * pesoa) + (b * pesob)) / 11;
     printf("MEDIA = %.5lf\n", mediap);
     return 0;
 }
